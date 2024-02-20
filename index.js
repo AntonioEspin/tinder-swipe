3058cfe18bb9e42c114a02e81ed8de0d7a154f2c
AntonioEspin <antoesdev91@gmail.com>
2024-02-20 17:09:28 -0500

    add logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('mousedown', startDrag)
 document.addEventListener('touchstart', startDrag, {passive: true})
 
+const DECISION_THRESHOLD = 175
 let isAnimating = false
 let pullDeltaX = 0
 
@@ -9,6 +10,8 @@ function startDrag (event) {
 
   // Obtener el primer articulo del elemento
   const actualCard = event.target.closest('article')
+
+  if(!actualCard) return
 
   const startX = event.pageX ?? event.touches[0].pageX
 
@@ -33,5 +36,50 @@ function startDrag (event) {
     actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`
 
     actualCard.style.cursor = 'grabbing'
+
+    // cambiar la opacidad 
+    const opacity = Math.abs(pullDeltaX) / 100
+    const isRight = pullDeltaX > 0
+
+    const choiceElement = isRight
+      ? actualCard.querySelector('.choice.like')
+      : actualCard.querySelector('.choice.nope')
+
+    choiceElement.style.opacity = opacity
+  }
+
+  function onEnd (event) {
+    document.removeEventListener('mousemove', onMove)
+    document.removeEventListener('mouseup', onEnd)
+
+    document.removeEventListener('touchmove', onMove)
+    document.removeEventListener('touchend', onMove)
+
+    const decisionMade = Math.abs(pullDeltaX) >= DECISION_THRESHOLD
+
+    if(decisionMade) {
+      const goRight = pullDeltaX >= 0
+      const goLeft = !goRight
+
+      // añadir clase acorde a la decision
+      actualCard.classList.add(goRight ? 'go-right': 'go-left')
+      actualCard.addEventListener('transitionend', ()=>{
+        actualCard.remove()
+      }, {once: true})
+    } else {
+      actualCard.classList.add('reset')
+      actualCard.classList.remove('go-right', 'go-left')
+    }
+
+    actualCard.addEventListener('transitionend', ()=> {
+      actualCard.removeAttribute('style')
+      actualCard.classList.remove('reset')
+      actualCard.querySelectorAll('.choice').forEach(el => {
+        el.style.opacity = 0
+      })
+
+      pullDeltaX = 0
+      isAnimating = false
+    })
   }
 }
